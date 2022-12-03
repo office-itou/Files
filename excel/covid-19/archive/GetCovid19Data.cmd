9526fa7952207f47d7ac5f8340c3fa433b6033ec
office-itou <itou-jun@outlook.com>
2022-12-03 23:33:50 +0900

    archived

---
+++ excel/covid-19/archive/GetCovid19Data.cmd
@@ -0,0 +1,73 @@
+@Echo Off
+
+    if not exist .\data\. mkdir .\data
+    if not exist .\conv\. mkdir .\conv
+
+    pushd .\data
+
+Rem https://github.com/office-itou/Files/tree/master/excel/covid-19
+    If Not Exist "人口(人口推計2019).csv" (
+        curl -L -# -R -S -o "人口(人口推計2019).csv" "https://raw.githubusercontent.com/office-itou/Files/master/excel/covid-19/Population(2019).csv"
+    )
+    If Not Exist "人口(国勢調査2020).csv" (
+        curl -L -# -R -S -o "人口(国勢調査2020).csv" "https://raw.githubusercontent.com/office-itou/Files/master/excel/covid-19/Population(2020).csv"
+    )
+
+Rem https://www3.nhk.or.jp/news/special/coronavirus/data/
+Rem curl -L -# -O -R -S "https://www3.nhk.or.jp/n-data/opendata/coronavirus/nhk_news_covid19_prefectures_daily_data.csv"
+Rem curl -L -# -O -R -S "https://www3.nhk.or.jp/n-data/opendata/coronavirus/nhk_news_covid19_domestic_daily_data.csv"
+
+Rem https://www.mhlw.go.jp/stf/covid-19/open-data.html
+Rem curl -L -# -O -R -S "https://www.mhlw.go.jp/content/severe_daily.csv"
+Rem curl -L -# -O -R -S "https://www.mhlw.go.jp/content/cases_total.csv"
+Rem curl -L -# -O -R -S "https://www.mhlw.go.jp/content/recovery_total.csv"
+Rem curl -L -# -O -R -S "https://www.mhlw.go.jp/content/pcr_case_daily.csv"
+Rem curl -L -# -O -R -S "https://www.mhlw.go.jp/content/death_total.csv"
+
+Rem PCR検査実施人数
+    curl -L -# -O -R -S "https://www.mhlw.go.jp/content/pcr_case_daily.csv"
+
+Rem PCR検査の実施件数
+    curl -L -# -O -R -S "https://www.mhlw.go.jp/content/pcr_tested_daily.csv"
+
+Rem 新規陽性者数の推移（日別）
+    curl -L -# -O -R -S "https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv"
+
+Rem 人口10万人当たり新規陽性者数
+    curl -L -# -O -R -S "https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_per_100_thousand_population_daily.csv"
+
+Rem 性別・年代別新規陽性者数（週別）
+    curl -L -# -O -R -S "https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_detail_weekly.csv"
+
+Rem 陽性者数（累積）
+    curl -L -# -O -R -S "https://covid19.mhlw.go.jp/public/opendata/confirmed_cases_cumulative_daily.csv"
+
+Rem 性別・年代別陽性者数（累積）
+    curl -L -# -O -R -S "https://covid19.mhlw.go.jp/public/opendata/confirmed_cases_detail_cumulative_weekly.csv"
+
+Rem 重症者数の推移
+    curl -L -# -O -R -S "https://covid19.mhlw.go.jp/public/opendata/severe_cases_daily.csv"
+
+Rem 性別・年代別重症者数
+    curl -L -# -O -R -S "https://covid19.mhlw.go.jp/public/opendata/severe_cases_detail_weekly.csv"
+
+Rem 死亡者数（累積）
+    curl -L -# -O -R -S "https://covid19.mhlw.go.jp/public/opendata/deaths_cumulative_daily.csv"
+
+Rem 性別・年代別死亡者数（累積）
+    curl -L -# -O -R -S "https://covid19.mhlw.go.jp/public/opendata/deaths_detail_cumulative_weekly.csv"
+
+Rem 入院治療等を要する者等推移
+    curl -L -# -O -R -S "https://covid19.mhlw.go.jp/public/opendata/requiring_inpatient_care_etc_daily.csv"
+
+Rem 集団感染等発生状況
+    curl -L -# -O -R -S "https://covid19.mhlw.go.jp/public/opendata/cluster_events_weekly.csv"
+
+Rem HER-SYSデータに基づく新規陽性者数
+    curl -L -# -O -R -S "https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_difference.csv"
+
+    popd
+
+    CScript ./MakeCovid19Graph.vbs
+
+Rem Pause.
